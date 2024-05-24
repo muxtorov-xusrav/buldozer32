f645aa233f37d102a6e94d0c1400c470b38f70e9
Farxodov0007 <uchqunbekf@gmail.com>
2024-05-24 15:26:22 +0500

    homeAbout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { Route,Routes } from "react-router-dom";
+
+Route
 function App() {
     return (
         <div className='App'>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+               
+                
+            </Routes>
             <h1>Hello</h1>
         </div>
     );
