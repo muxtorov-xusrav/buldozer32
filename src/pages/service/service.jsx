edf8347bc10e4a51920ac72ecbe8568dc404f807
xushnudbek0521 <hushnudbek.pubger555@gmail.com>
2024-06-04 14:52:27 +0500

    first commit

--- src/pages/service/service.jsx
+++ src/pages/service/service.jsx
@@ -1,11 +1,131 @@
 import React from 'react'
+import img from '../../assets/portfolio-1.jpg'
+import AddIcon from '@mui/icons-material/Add';
+import './style.css'
+function About() {
+    return (
+        <div>
+            <div className="about">
+                <h2 className='h2'>
+                    Our Services
+                    <div style={{ display: 'flex', justifyContent: 'center' }}>
 
-const service = () => {
-  return (
-    <div>
-      service
-    </div>
-  )
+                        <hr style={{ width: 60, border: '2px solid #030f27' }} />
+                    </div>
+                </h2>
+
+                <div className='pages'>
+                    <div className="home">Home</div>
+                    <div className="slesh" >/</div>
+                    <div className="about-us">Our Services</div>
+                </div>
+
+            </div>
+
+
+
+
+            <div className="ourser">
+                <h4 className='p'>Our Services</h4>
+                <h2 className='h2-w'>We Provide Services</h2>
+            </div>
+
+
+            <div className="Cards">
+                <div className="card">
+                    <img width={360} height={420} src={img} alt="" />
+                    <div style={{ background: '#030F27', color: '#FDBE33',display:'flex',alignItems:'center',height:60,justifyContent:'space-between' }}><h3>Building Construction</h3><button style={{fontSize:60,color:'black',background:'#fdbe33',width:60,height:60}}><AddIcon sx={{marginTop:-6}}/></button></div>
+                </div>
+                <div className="card">
+                    <img width={360} height={420} src={img} alt="" />
+                    <div style={{ background: '#030F27', color: '#FDBE33',display:'flex',alignItems:'center',height:60,justifyContent:'space-between' }}><h3>House Renovation</h3><button style={{fontSize:60,color:'black',background:'#fdbe33',width:60,height:60}}><AddIcon sx={{marginTop:-6}}/></button></div>
+                </div>
+                <div className="card">
+                    <img width={360} height={420} src={img} alt="" />
+                    <div style={{ background: '#030F27', color: '#FDBE33',display:'flex',alignItems:'center',height:60,justifyContent:'space-between' }}><h3>Architecture Design</h3><button style={{fontSize:60,color:'black',background:'#fdbe33',width:60,height:60}}><AddIcon sx={{marginTop:-6}}/></button></div>
+                </div>
+            </div>
+            <div className="Cards" style={{paddingTop:30,paddingBottom:80}}>
+                <div className="card">
+                    <img width={360} height={420} src={img} alt="" />
+                    <div style={{ background: '#030F27', color: '#FDBE33',display:'flex',alignItems:'center',height:60,justifyContent:'space-between' }}><h3>Interior Design</h3><button style={{fontSize:60,color:'black',background:'#fdbe33',width:60,height:60}}><AddIcon sx={{marginTop:-6}}/></button></div>
+                </div>
+                <div className="card">
+                    <img width={360} height={420} src={img} alt="" />
+                    <div style={{ background: '#030F27', color: '#FDBE33',display:'flex',alignItems:'center',height:60,justifyContent:'space-between' }}><h3>Fixing & Support</h3><button style={{fontSize:60,color:'black',background:'#fdbe33',width:60,height:60}}><AddIcon sx={{marginTop:-6}}/></button></div>
+                </div>
+                <div className="card">
+                    <img width={360} height={420} src={img} alt="" />
+                    <div style={{ background: '#030F27', color: '#FDBE33',display:'flex',alignItems:'center',height:60,justifyContent:'space-between' }}><h3>Painting</h3><button style={{fontSize:60,color:'black',background:'#fdbe33',width:60,height:60}}><AddIcon sx={{marginTop:-6}}/></button></div>
+                </div>
+            </div>
+
+
+            <div className="new2">
+                <p style={{ fontSize: '18px', color: '#FDBE33', textAlign: 'center' }}>Frequently Asked Question</p>
+                <h2 style={{ fontSize: '50px', color: '#030ff27', textAlign: 'center' }}>You May Ask</h2>
+
+                <div className='new'>
+                    <div className='new1'>
+                        <div>
+                            <div style={{ paddingTop: 15, display: 'flex', justifyContent: 'space-between' }}>
+                                <div style={{ border: '1px solid black', padding: '15px 25px', fontSize: '16px', width: 500, display: 'flex', justifyContent: 'space-between' }}>Lorem ipsum dolor sit amet.       <div>+</div></div>
+
+                            </div>
+                            <div style={{ paddingTop: 15, display: 'flex', justifyContent: 'space-between' }}>
+                                <div style={{ border: '1px solid black', padding: '15px 25px', fontSize: '16px', width: 500, display: 'flex', justifyContent: 'space-between' }}>Lorem ipsum dolor sit amet.       <div>+</div></div>
+
+                            </div>
+                            <div style={{ paddingTop: 15, display: 'flex', justifyContent: 'space-between' }}>
+                                <div style={{ border: '1px solid black', padding: '15px 25px', fontSize: '16px', width: 500, display: 'flex', justifyContent: 'space-between' }}>Lorem ipsum dolor sit amet.       <div>+</div></div>
+
+                            </div>
+                            <div style={{ paddingTop: 15, display: 'flex', justifyContent: 'space-between' }}>
+                                <div style={{ border: '1px solid black', padding: '15px 25px', fontSize: '16px', width: 500, display: 'flex', justifyContent: 'space-between' }}>Lorem ipsum dolor sit amet.       <div>+</div></div>
+
+                            </div>
+                            <div style={{ paddingTop: 15, display: 'flex', justifyContent: 'space-between' }}>
+                                <div style={{ border: '1px solid black', padding: '15px 25px', fontSize: '16px', width: 500, display: 'flex', justifyContent: 'space-between' }}>Lorem ipsum dolor sit amet.       <div>+</div></div>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="new12"></div>
+
+
+                    <div className='new1'>
+                        <div>
+                            <div style={{ paddingTop: 15, display: 'flex', justifyContent: 'space-between' }}>
+                                <div style={{ border: '1px solid black', padding: '15px 25px', fontSize: '16px', width: 500, display: 'flex', justifyContent: 'space-between' }}>Lorem ipsum dolor sit amet.       <div>+</div></div>
+
+                            </div>
+                            <div style={{ paddingTop: 15, display: 'flex', justifyContent: 'space-between' }}>
+                                <div style={{ border: '1px solid black', padding: '15px 25px', fontSize: '16px', width: 500, display: 'flex', justifyContent: 'space-between' }}>Lorem ipsum dolor sit amet.       <div>+</div></div>
+
+                            </div>
+                            <div style={{ paddingTop: 15, display: 'flex', justifyContent: 'space-between' }}>
+                                <div style={{ border: '1px solid black', padding: '15px 25px', fontSize: '16px', width: 500, display: 'flex', justifyContent: 'space-between' }}>Lorem ipsum dolor sit amet.       <div>+</div></div>
+
+                            </div>
+                            <div style={{ paddingTop: 15, display: 'flex', justifyContent: 'space-between' }}>
+                                <div style={{ border: '1px solid black', padding: '15px 25px', fontSize: '16px', width: 500, display: 'flex', justifyContent: 'space-between' }}>Lorem ipsum dolor sit amet.       <div>+</div></div>
+
+                            </div>
+                            <div style={{ paddingTop: 15, display: 'flex', justifyContent: 'space-between' }}>
+                                <div style={{ border: '1px solid black', padding: '15px 25px', fontSize: '16px', width: 500, display: 'flex', justifyContent: 'space-between' }}>Lorem ipsum dolor sit amet.       <div>+</div></div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+
+
+        </div>
+    )
 }
 
-export default service
+export default About
